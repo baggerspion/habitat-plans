b3f07153866eb5690585c130d54bf2e343303a31
Paul Adams <paul@baggerspion.net>
2017-10-15 14:41:54 +0200

    Update tcl: should build with pkgconfig support

--- tcl/plan.sh
+++ tcl/plan.sh
@@ -3,10 +3,12 @@ pkg_origin=baggerspion
 pkg_version="8.6.7"
 pkg_maintainer="Paul Adams <paul@baggerspion.net>"
 pkg_license=('custom')
+pkg_dirname=${pkg_name}${pkg_version}
 pkg_source="http://downloads.sourceforge.net/sourceforge/${pkg_name}/${pkg_name}${pkg_version}-src.tar.gz"
-pkg_shasum="TODO"
+pkg_shasum="7c6b8f84e37332423cfe5bae503440d88450da8cc1243496249faa5268026ba5"
 pkg_upstream_url="http://www.tcl.tk/"
 pkg_description="Tool Command Language -- A dynamic programming language."
+pkg_pconfig_dirs=(lib/pkgconfig)
 pkg_deps=(
   core/gcc-libs 
   core/glibc 
@@ -25,12 +27,23 @@ pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
 
 do_build() {
-  ./configure --prefix="$pkg_prefix" \
-              --enable-threads \
-              --enable-64bit
-  make
+  pushd unix > /dev/null
+    ./configure --prefix="$pkg_prefix" \
+                --enable-threads \
+                --enable-64bit
+    make
+  popd > /dev/null
+}
+
+do_install() {
+  pushd unix > /dev/null
+    make install
+    make install-private-headers
+  popd > /dev/null
 }
 
 do_check() {
-  make test
+  pushd unix > /dev/null
+    make test
+  popd > /dev/null
 }
